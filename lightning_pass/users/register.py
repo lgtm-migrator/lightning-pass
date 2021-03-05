040da7a602dc7da756f4a81a88f235d2569ad648
Lukas Kucera <lukas.kucera.g@protonmail.com>
2021-03-05 10:41:14 +0100

    custom exceptions better error handling in credentials_eligibility

--- lightning_pass/users/register.py
+++ lightning_pass/users/register.py
@@ -3,6 +3,13 @@ import re
 
 import mysql.connector as mysql
 from dotenv import load_dotenv
+from exceptions import (
+    EmailAlreadyExists,
+    InvalidEmail,
+    InvalidPassword,
+    InvalidUsername,
+    UsernameAlreadyExists,
+)
 
 REGEX_EMAIL = r"^[a-z0-9]+[._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
 
@@ -30,51 +37,53 @@ class RegisterUser:
 
     @staticmethod
     def check_username(username):
-        sql = f"SELECT 1 FROM lightning_pass.credentials WHERE username = '{username}'"
-        cursor.execute(sql)
+        """Check whether a username already exists in a database and if it matches a required pattern."""
+        sql = "SELECT 1 FROM lightning_pass.credentials WHERE username = VALUES (%s)"
+        val = username
+        cursor.execute(sql, val)
         row = cursor.fetchall()
-        if len(row) < 1:
-            return True
-        else:
-            return False
+
+        if len(row) <= 0:
+            raise UsernameAlreadyExists
+        elif len(username) < 5:
+            raise InvalidUsername
+
+    @staticmethod
+    def check_password(password):
+        if (
+            len(password) < 8
+            or len(re.findall(r"[A-Z]", password)) <= 0
+            or len(re.findall(r"[0-9~!@#$%^&*()_+/[]{}:'\"<>?|;-\\]", password)) <= 0
+        ):
+            raise InvalidPassword
 
     @staticmethod
     def check_email(email):
-        sql = f"SELECT 1 FROM lightning_pass.credentials WHERE email = '{email}'"
-        cursor.execute(sql)
+        """Check whether an email already exists and if it matches a correct email pattern."""
+        sql = "SELECT 1 FROM lightning_pass.credentials WHERE email = VALUES (%s)"
+        val = email
+        cursor.execute(sql, val)
         row = cursor.fetchall()
-        if len(row) < 1:
-            return True
-        else:
-            return False
+
+        if len(row) <= 0:
+            raise EmailAlreadyExists
+        elif not re.search(REGEX_EMAIL, email):
+            raise InvalidEmail
 
     def credentials_eligibility(self):
-        """ Used to check whether submitted credentials meet the desired requirements. """
-        if (
-            not re.search(REGEX_EMAIL, self.email)
-            or not len(re.findall('[0-9/.,*+-`"a-zA-Z]', self.username)) > 5
-            or not len(re.findall("[A-Z]", self.password)) != 0
-            or not len(re.findall('[0-9/.,*+-`"]', self.password)) != 0
-            or not len(re.findall('[0-9/.,*+-`"a-zA-Z]', self.password)) > 8
-        ):
-            print("false")
-            return False
-        else:
-            if not self.check_username(self.username):
-                return "username exists"
-            elif not self.check_email(self.email):
-                return "email exists"
-            else:
-                return True
+        """Used to check whether submitted credentials meet the desired requirements."""
+        self.check_username(self.username)
+        self.check_password(self.password)
+        self.check_email(self.email)
 
     def insert_into_db(self):
-        if self.credentials_eligibility():
-            sql = "INSERT INTO lightning_pass.credentials (username, password, email) VALUES (%s, %s, %s)"
-            val = [self.username, self.password, self.email]
-            cursor.execute(sql, val)
-            connection.commit()
-        else:
-            return False
+        """Insert user into a database.
+        If credentials check fails, a custom exception is raised."""
+        self.credentials_eligibility()
+        sql = "INSERT INTO lightning_pass.credentials (username, password, email) VALUES (%s, %s, %s)"
+        val = [self.username, self.password, self.email]
+        cursor.execute(sql, val)
+        connection.commit()
 
 
 user = RegisterUser("usernam", "passwordLL12", "emai@email.com")
