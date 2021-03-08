8c74526959e21a09df0b6e4f8b584df9acf4123c
Lukas Kucera <lukas.kucera.g@protonmail.com>
2021-03-08 14:24:45 +0100

    Create database if not exists

--- lightning_pass/__init__.py
+++ lightning_pass/__init__.py
@@ -10,7 +10,7 @@ from lightning_pass.gui.gui import UiLightningPass
 
 
 def _copy(self, target):
-    """Monkey Patching copy functionality into pathlib.Path object."""
+    """Monkey Patch copy functionality into pathlib.Path object."""
     import shutil
 
     assert self.is_file()
@@ -21,7 +21,12 @@ Path.copy = _copy  # type: ignore
 
 
 def connect_to_database():
-    """Initialize database connection."""
+    """Initialize database connection.
+
+    :returns cursor, connection
+    :rtype tuple
+
+    """
     load_dotenv()
     connection = mysql.connect(
         host=os.getenv("LOGINSDB_HOST"),
@@ -29,11 +34,33 @@ def connect_to_database():
         password=os.getenv("LOGINSDB_PASS"),
         database=os.getenv("LOGINSDB_DB"),
     )
-    return connection.cursor(), connection
+    cursor = connection.cursor()
+    sql = """CREATE TABLE if not exists credentials(
+            `id` int NOT NULL AUTO_INCREMENT,
+            `username` varchar(255) NOT NULL,
+            `password` varchar(255) NOT NULL,
+            `email` varchar(255) NOT NULL,
+            `profile_picture` varchar(255) NOT NULL DEFAULT 'default.png',
+            `last_login_date` timestamp NULL DEFAULT NULL,
+            `register_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+             PRIMARY KEY(`id`)
+             )
+             ENGINE = InnoDB
+             AUTO_INCREMENT = 1
+             DEFAULT CHARSET = utf8mb4
+             COLLATE = utf8mb4_0900_ai_ci
+             """
+    cursor.execute(sql)
+    return cursor, connection
 
 
 def create_app():
-    """App creation"""
+    """App creation.
+
+    :returns app: app instance
+    :rtype QApplication
+
+    """
     app = QApplication(sys.argv)
     window = UiLightningPass()
     window.show()
