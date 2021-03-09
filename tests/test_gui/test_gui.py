970b0a501988858ab95bc7f20c0e7c4105548936
Lukas Kucera <lukas.kucera.g@protonmail.com>
2021-03-09 10:24:09 +0100

    Parametrize tests and set it up with new window

--- tests/test_gui/test_gui.py
+++ tests/test_gui/test_gui.py
@@ -1,66 +1,51 @@
 import pytest
 from PyQt5 import QtCore
+from PyQt5.QtWidgets import QPushButton
+from pytestqt.qtbot import QtBot
 
-from lightning_pass.gui.gui import UiLightningPass
+from lightning_pass.gui.gui2 import LightningPassWindow
 
 
 @pytest.fixture
 def app(qtbot):
-    test_app = UiLightningPass()
+    test_app = LightningPassWindow()
     qtbot.addWidget(test_app)
     return test_app
 
 
-"""
-Test if each button correctly switches to correct stackedWidget.
+@pytest.mark.parametrize(
+    "widget, index",
+    [
+        ("home_login_btn", 1),
+        ("home_register_btn", 2),
+        ("home_generate_password_btn", 4),
+        ("log_main_btn", 0),
+        ("log_forgot_pass_btn", 3),
+        ("reg_main_btn", 0),
+        ("forgot_pass_main_menu_btn", 0),
+        ("generate_pass_main_menu_btn", 0),
+        ("generate_pass_p2_main_btn", 0),
+        ("account_main_menu_btn", 0),
+    ],
+)
+def test_buttons(
+    app: LightningPassWindow, qtbot: QtBot, widget: QPushButton, index: int
+) -> None:
+    """Test if each button correctly switches to correct stackedWidget.
 
-Index description:
-Index 0: app.home
-Index 1: app.login
-Index 2: app.register_2
-Index 3: app.forgot_password
-Index 4: app.generate_password
-Index 5: app.generate_pass_phase2
-Index 6: app.account
-"""
+    :param LightningPassWindow app: main window instance
+    :param QtBot qtbot: QtBot instance
+    :param QPushButton widget: QPushButton instance
+    :param int index: stacked_widget expected index,
+        index 0: app.ui.home
+        index 1: app.ui.login
+        index 2: app.ui.register_2
+        index 3: app.ui.forgot_password
+        index 4: app.ui.generate_password
+        index 5: app.ui.generate_pass_phase2
+        index 6: app.ui.account
 
-
-def test_home_buttons(app, qtbot):
-    qtbot.mouseClick(app.home_login_btn, QtCore.Qt.LeftButton)
-    assert app.stacked_widget.currentIndex() == 1
-    qtbot.mouseClick(app.home_register_btn, QtCore.Qt.LeftButton)
-    assert app.stacked_widget.currentIndex() == 2
-    qtbot.mouseClick(app.home_generate_password_btn, QtCore.Qt.LeftButton)
-    assert app.stacked_widget.currentIndex() == 4
-
-
-def test_login_buttons(app, qtbot):
-    qtbot.mouseClick(app.log_main_btn, QtCore.Qt.LeftButton)
-    assert app.stacked_widget.currentIndex() == 0
-    qtbot.mouseClick(app.log_forgot_pass_btn, QtCore.Qt.LeftButton)
-    assert app.stacked_widget.currentIndex() == 3
-
-
-def test_register_2_buttons(app, qtbot):
-    qtbot.mouseClick(app.reg_main_btn, QtCore.Qt.LeftButton)
-    assert app.stacked_widget.currentIndex() == 0
-
-
-def test_forgot_password_buttons(app, qtbot):
-    qtbot.mouseClick(app.forgot_pass_main_menu_btn, QtCore.Qt.LeftButton)
-    assert app.stacked_widget.currentIndex() == 0
-
-
-def test_generate_pass_buttons(app, qtbot):
-    qtbot.mouseClick(app.generate_pass_main_menu_btn, QtCore.Qt.LeftButton)
-    assert app.stacked_widget.currentIndex() == 0
-
-
-def test_generate_pass_phase2_buttons(app, qtbot):
-    qtbot.mouseClick(app.generate_pass_p2_main_btn, QtCore.Qt.LeftButton)
-    assert app.stacked_widget.currentIndex() == 0
-
-
-def test_account_buttons(app, qtbot):
-    qtbot.mouseClick(app.account_main_menu_btn, QtCore.Qt.LeftButton)
-    assert app.stacked_widget.currentIndex() == 0
+    """
+    widget = getattr(app.ui, widget)
+    qtbot.mouseClick(widget, QtCore.Qt.LeftButton)
+    assert app.ui.stacked_widget.currentIndex() == index
