2ccf6ac320306d71f5be0a087561153c781789e1
Lukas Kucera <lukas.kucera.g@protonmail.com>
2021-03-09 17:36:26 +0100

    Document and refactor message_boxes.py

--- lightning_pass/gui/message_boxes.py
+++ lightning_pass/gui/message_boxes.py
@@ -1,9 +1,13 @@
+"""This module holds the MessageBoxes class used for showing various message boxes."""
+
 import re
 
-from PyQt5.QtWidgets import QMessageBox, QWidget
+from PyQt5.QtWidgets import QMessageBox, QPushButton, QWidget
 
 
 class MessageBoxes(QWidget):
+    """This class holds the functionality to show various message boxes."""
+
     def __init__(self, child, parent):
         """Class constructor."""
         super().__init__(parent)
@@ -11,100 +15,178 @@ class MessageBoxes(QWidget):
         self.main_win = child
         self.title = "Lightning Pass"
 
-    def invalid_username_box(self, widget):
-        self.show_message_box(
-            f"{self.title} - {widget}",
-            "This username is invalid.",
-            QMessageBox.Warning,
+    def invalid_username_box(self, parent: str) -> None:
+        """Show invalid username message box.
+
+        :param str parent: specifies which window insantiated current box
+
+        """
+        box = QMessageBox(self.main_win)
+        box.setWindowTitle(f"{self.title} - {parent}")
+        box.setText("This username is invalid.")
+        box.setIcon(QMessageBox.Warning)
+
+        box.setInformativeText("Username be at least 5 characters long.")
+        box.exec_()
+
+    def invalid_password_box(self, parent: str) -> None:
+        """Show invalid password message box.
+
+        :param str parent: specifies which window insantiated current box
+
+        """
+        box = QMessageBox(self.main_win)
+        box.setWindowTitle(f"{self.title} - {parent}")
+        box.setText("This password is invalid.")
+        box.setIcon(QMessageBox.Warning)
+
+        box.setInformativeText(
+            """Password must be at least 8 characters long,
+contain atleast 1 capital letter,
+contain atleast one special character.
+"""
         )
+        box.exec_()
 
-    def invalid_password_box(self, widget):
-        self.show_message_box(
-            f"{self.title} - {widget}",
-            "This password is invalid.",
-            QMessageBox.Warning,
-        )
+    def invalid_email_box(self, parent: str) -> None:
+        """Show invalid email message box.
 
-    def invalid_email_box(self, widget):
-        self.show_message_box(
-            f"{self.title} - {widget}",
-            "This email is invalid.",
-            QMessageBox.Warning,
-        )
+        :param str parent: specifies which window insantiated current box
 
-    def invalid_login_box(self, widget):
-        self.show_message_box(
-            f"{self.title} - {widget}",
-            "An account with that username or password doesn't exist.",
-            QMessageBox.Warning,
-        )
+        """
+        box = QMessageBox(self.main_win)
+        box.setWindowTitle(f"{self.title} - {parent}")
+        box.setText("Please enter a valid email.")
+        box.setIcon(QMessageBox.Warning)
+        box.exec_()
 
-    def username_already_exists_box(self, widget):
-        self.show_message_box(
-            f"{self.title} - {widget}",
-            "This username already exists.",
-            QMessageBox.Warning,
-        )
+    def invalid_login_box(self, parent: str) -> None:
+        """Show invalid login message box.
 
-    def email_already_exists_box(self, widget):
-        self.show_message_box(
-            f"{self.title} - {widget}",
-            "This email already exists.",
-            QMessageBox.Warning,
-        )
+        :param str parent: specifies which window insantiated current box
 
-    def passwords_do_not_match_box(self, widget):
-        self.show_message_box(
-            f"{self.title} - {widget}",
-            "The passwords you entered do not match.",
-            QMessageBox.Warning,
-        )
+        """
 
-    def account_creation_box(self, widget):
-        self.show_message_box(
-            f"{self.title} - {widget}",
-            "Account successfully created!",
-            QMessageBox.Question,
-            successful_registration=True,
-        )
+        def event_handler(btn: QPushButton) -> None:
+            """Handler for clicks on message box window
 
-    def no_case_type_box(self, widget):
-        self.show_message_box(
-            f"{self.title} - {widget}",
-            "Can't generate password without any case type.",
-            QMessageBox.Warning,
-        )
+            :param btn: clicked button
 
-    def login_required_box(self, widget):
-        self.show_message_box(
-            f"{self.title} - {widget}",
-            "Please log in to access that page.",
-            QMessageBox.Warning,
-        )
+            """
+            if re.findall("Yes", btn.text()):
+                self.events.forgot_password_event()
+            elif re.findall("No", btn.text()):
+                self.events.login_event()
 
-    def details_updated_box(self, detail, widget):
-        self.show_message_box(
-            f"{self.title} - {widget}",
-            f"Your {detail} has been successfully updated!",
-            QMessageBox.Question,
-        )
+        box = QMessageBox(self.main_win)
+        box.setWindowTitle(f"{self.title} - {parent}")
+        box.setText("Invalid login details.")
+        box.setIcon(QMessageBox.Warning)
 
-    def message_box_event(self, btn):
-        """Handler for clicks on message box window"""
-        if re.findall("Yes", btn.text()):
-            self.events.login_event()
-        if re.findall("Cancel", btn.text()):
-            self.events.register_event()
+        box.setInformativeText("Forgot password?")
+        box.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        box.setDefaultButton(QMessageBox.Yes)
+        box.buttonClicked.connect(event_handler)
+        box.exec_()
 
-    def show_message_box(self, title, text, icon, successful_registration=False):
-        """Show message box with information about registration process"""
-        er = QMessageBox(self.main_win)
-        er.setWindowTitle(title)
-        er.setText(text)
-        er.setIcon(icon)
-        if successful_registration is True:
-            er.setInformativeText("Would you like to move to the login page?")
-            er.setStandardButtons(QMessageBox.Yes | QMessageBox.Close)
-            er.setDefaultButton(QMessageBox.Yes)
-            er.buttonClicked.connect(self.message_box_event)
-        _ = er.exec_()
+    def username_already_exists_box(self, parent: str) -> None:
+        """Show username already exists message box.
+
+        :param str parent: specifies which window insantiated current box
+
+        """
+        box = QMessageBox(self.main_win)
+        box.setWindowTitle(f"{self.title} - {parent}")
+        box.setText("This username already exists.")
+        box.setIcon(QMessageBox.Warning)
+        box.exec_()
+
+    def email_already_exists_box(self, parent: str) -> None:
+        """Show email already exists message box.
+
+        :param str parent: specifies which window insantiated current box
+
+        """
+        box = QMessageBox(self.main_win)
+        box.setWindowTitle(f"{self.title} - {parent}")
+        box.setText("This email already exists.")
+        box.setIcon(QMessageBox.Warning)
+        box.exec_()
+
+    def passwords_do_not_match_box(self, parent: str) -> None:
+        """Show passwords do not match message box.
+
+        :param str parent: specifies which window insantiated current box
+
+        """
+        box = QMessageBox(self.main_win)
+        box.setWindowTitle(f"{self.title} - {parent}")
+        box.setText("Passwords don't match.")
+        box.setIcon(QMessageBox.Warning)
+        box.exec_()
+
+    def account_creation_box(self, parent: str) -> None:
+        """Show successful account creatio message box.
+
+        :param str parent: specifies which window insantiated current box
+
+        """
+
+        def event_handler(btn: QPushButton) -> None:
+            """Handler for clicks on message box window
+
+            :param btn: clicked button
+
+            """
+            if re.findall("Yes", btn.text()):
+                self.events.login_event()
+            elif re.findall("No", btn.text()):
+                self.events.register_event()
+
+        box = QMessageBox(self.main_win)
+        box.setWindowTitle(parent)
+        box.setText("Account successfuly created.")
+        box.setIcon(QMessageBox.Question)
+
+        box.setInformativeText("Would you like to move to the login page?")
+        box.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        box.setDefaultButton(QMessageBox.Yes)
+        box.buttonClicked.connect(event_handler)
+        box.exec_()
+
+    def no_case_type_box(self, parent: str) -> None:
+        """Show message box indicating that password can't be generated with current case type option.
+
+        :param str parent: specifies which window insantiated current box
+
+        """
+        box = QMessageBox(self.main_win)
+        box.setWindowTitle(f"{self.title} - {parent}")
+        box.setText("Passwords can't be generate without any case type.")
+        box.setIcon(QMessageBox.Warning)
+        box.exec_()
+
+    def login_required_box(self, parent: str) -> None:
+        """Show message box indicating that password can't be generated with current case type option.
+
+        :param str parent: specifies which window insantiated current box
+
+        """
+        box = QMessageBox(self.main_win)
+        box.setWindowTitle(f"{self.title} - {parent}")
+        box.setText("Please log in to access that page.")
+        box.setIcon(QMessageBox.Warning)
+        box.exec_()
+
+    def details_updated_box(self, detail: str, parent: str) -> None:
+        """Show message box indicating that password can't be generated with current case type option.
+
+        :param str detail: specifies which detail was updated
+        :param str parent: specifies which window insantiated current box
+
+        """
+        box = QMessageBox(self.main_win)
+        box.setWindowTitle(f"{self.title} - {parent}")
+        box.setText(f"Your {detail} has been successfully updated!")
+        box.setIcon(QMessageBox.Question)
+        box.exec_()
