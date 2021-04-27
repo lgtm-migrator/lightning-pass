21c9340daf738384c4850b4d49bf86beaecd68f2
Lukas Kucera <lukas.kucera.g@protonmail.com>
2021-04-27 19:51:15 +0200

    Update gitlab ci

--- lightning_pass/gui/gui_config/__init__.py
+++
@@ -1,2 +0,0 @@
-"""Subpackage containing modules to setup the GUI elements."""
-__all__ = ["buttons", "event_decorators", "widgets"]
---
+++ lightning_pass/gui/gui_util/__init__.py
@@ -0,0 +1,2 @@
+"""Subpackage containing helper modules to work with the GUI elements."""
+__all__ = ["buttons", "event_decorators", "widgets"]
